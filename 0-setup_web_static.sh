3e2556e957b12312cc6b3ac2901dedc9b5a35e9b
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-01-10 18:42:28 +0000

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #setsup webser to create a website using nginx
-sudo apt-get -y update
+sudo apt-get update
 sudo apt-get -y install nginx
 sudo service nginx start
 sudo mkdir -p /data/
@@ -19,8 +19,8 @@ echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo chown -R ubuntu:ubuntu /data/
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sed -i '48i\\n\tlocation /hbnb_static/ {' /etc/nginx/sites-available/default
-sed -i '50i\\t\talias /data/web_static/current/;' /etc/nginx/sites-available/default
-sed -i '51i\\t}' /etc/nginx/sites-available/default
+sudo sed -i '48i\\n\tlocation /hbnb_static/ {' /etc/nginx/sites-available/default
+sudo sed -i '50i\\t\talias /data/web_static/current/;' /etc/nginx/sites-available/default
+sudo sed -i '51i\\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
 exit 0
