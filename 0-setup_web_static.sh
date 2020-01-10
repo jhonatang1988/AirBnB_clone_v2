9dfb00b6871bc510768cc76a67d4df74f9033f61
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-01-10 20:18:33 +0000

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -16,9 +16,6 @@ echo "<html>
 sudo chown -R ubuntu:ubuntu /data/
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-sudo sed -i '43i\\n' /etc/nginx/sites-available/default
-sudo sed -i '44i\\tlocation /hbnb_static {' /etc/nginx/sites-available/default
-sudo sed -i '45i\\t\talias /data/web_static/current;' /etc/nginx/sites-available/default
-sudo sed -i '46i\\t}' /etc/nginx/sites-available/default
+sudo sed -i "43i\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}" /etc/nginx/sites-available/default
 sudo service nginx restart
 exit 0
