6c34e3f336337fb515ad3a685a86e55d167d728b
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-01-10 20:27:07 +0000

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,6 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 ufw allow 'Nginx HTTP'
-sudo service nginx start
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 echo "<html>
@@ -13,9 +12,8 @@ echo "<html>
     Probando el pollo
   </body>
 </html>" > /data/web_static/releases/test/index.html
-sudo chown -R ubuntu:ubuntu /data/
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 sudo sed -i "43i\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sudo chown -R ubuntu:ubuntu /data/
 sudo service nginx restart
 exit 0
