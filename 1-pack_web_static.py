8c4bac120d15e55e3d4b8a1365df0174f3a3f230
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-01-10 21:25:12 +0000

    11

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-"""Pack web static"""
-from fabric.api import local
-from datetime import datetime
+# build a tgz for fabfile
+import tarfile
+import os
+import time
+from fabric.api import *
 
 
 def do_pack():
-    """Compress files"""
+    print("hola")
+    timestr = time.strftime("%Y%m%d%H%M%S")
+    tgzfilename = 'versions/web_static_{}.tgz'.format(timestr)
+    if not os.path.exists('versions'):
+        try:
+            local('mkdir versions')
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
     try:
-        local("mkdir -p versions")
-        now = datetime.now()
-        current_date = now.strftime("%Y%m%d%H%M%S")
-        f_path = "versions/web_static_" + current_date
-        local("tar -cvzf {}.tgz web_static"
-              .format(f_path))
-        return f_path
+        with tarfile.open(tgzfilename, "w:gz") as tgz:
+            local(tgz.add('web_static',
+                          arcname=os.path.basename('web_static')))
+            return name
     except:
         return None
