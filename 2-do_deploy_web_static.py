1779835239b68be3c4de3c2858449ec15149fc24
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-01-10 23:53:16 +0000

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,18 +16,19 @@ def do_deploy(archive_path):
             rfn = archive_path[9:-4]
             tarn = archive_path[9:]
             folder = "mkdir -p /data/web_static/releases/{}".format(rfn)
-            foldername = "/data/web_static/releases/{}".format(rfn)
+            foldername = "/data/web_static/releases/{}/".format(rfn)
             run(folder)
             put(archive_path, '/tmp/')
             untar = "tar -C {} \
             -xzf /tmp/{}".format(foldername, tarn)
             run(untar)
-            run("rm /tmp/{}".format(tarn))
-            run("mv {}/web_static/* {}".format(foldername, foldername))
-            run("rm -rf {}/web_static".format(foldername))
-            run("rm -rf /data/web_static/current")
-            run("ln -s {} /data/web_static/current")
+            run("sudo rm /tmp/{}".format(tarn))
+            run("sudo mv {}/web_static/* {}".format(foldername, foldername))
+            run("sudo rm -rf {}/web_static".format(foldername))
+            run("sudo rm -rf /data/web_static/current")
+            run("sudo ln -s {} /data/web_static/current".format(foldername))
             print("New version deployed!")
+            return True
         else:
             print("no encontro")
             return False
