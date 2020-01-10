3a91598216a84a6e7f803fd559cd9b41e6b15e59
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-01-10 18:29:33 +0000

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,8 +3,13 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo service nginx start
-sudo mkdir -p /data/ /data/{web_static/,web_static/releases/,web_static/shared/,web_static/releases/test/}
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 sudo chown -R ubuntu:ubuntu /data/
+sudo touch /data/web_static/releases/test/index.html
 echo "<html>
   <head>
   </head>
@@ -12,6 +17,7 @@ echo "<html>
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
+sudo chown -R ubuntu:ubuntu /data/
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sed -i '48i\\n\tlocation /hbnb_static/ {' /etc/nginx/sites-available/default
 sed -i '50i\\t\talias /data/web_static/current/;' /etc/nginx/sites-available/default
