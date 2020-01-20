d13eba71c4df70a6d548fa2eee2eb650c3cd4377
Jhonatan Galindo <jhonatan1988@gmail.com>
2020-01-20 23:06:07 +0000

    0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+starts an flask app
+"""
+from flask import Flask
+
+if __name__ == '__main__':
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        app.strict_slashes=False
+        return 'Hello HBNB!'
+
+    app.run()
---
+++ web_flask/README.md
@@ -0,0 +1,2 @@
+# about
+## flask web framework implementation
